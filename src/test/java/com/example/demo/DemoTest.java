50dc7eb52f8dfb1233ca109f86d8934ddf82096d
wangyuanyuan <wangyuanyuan@thadhealth.com>
2021-02-04 17:32:19 +0800

    提交测试poi导入

--- src/test/java/com/example/demo/DemoTest.java
+++ src/test/java/com/example/demo/DemoTest.java
@@ -1,11 +1,17 @@
 package com.example.demo;
 
+import com.example.demo.entity.User;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -101,7 +107,44 @@ public class DemoTest {
             System.out.println(hashKey + ": " + resultMap.get(hashKey));
         }
     }
+/**
+ * @Author: Wyy
+ * @Date: 2021/2/4 16:35
+ * @Description: 功能描述 测试Excel导入库里
+ * @Param: []
+ * @Return: void
+ */
+    @Test
+    public void testExcelInMysql() throws Exception {
+        //        1.通过流读取Excel文件 
+         FileInputStream inputStream = new FileInputStream("D:\\file\\yy.xlsx");
+        //         2.通过poi解析流 HSSFWorkbook 处理流得到的对象中 就封装了Excel文件所有的数据    
+         HSSFWorkbook workbook=new HSSFWorkbook(inputStream);
+        //         3.从文件中获取表对象  getSheetAt通过下标获取
+         HSSFSheet sheet=workbook.getSheetAt(0);
+        //         4.从表中获取到行数据  从第二行开始 到 最后一行  getLastRowNum() 获取最后一行的下标 
+         int lastRowNum=sheet.getLastRowNum();
+         for(int i=1;i<=lastRowNum;i++){
+        //             通过下标获取行          
+         HSSFRow row=sheet.getRow(i);
+        //             从行中获取数据         
+        //    /**             * getNumericCellValue() 获取数字             * getStringCellValue 获取String             */    
+         double id=row.getCell(0).getNumericCellValue();
+         String name=row.getCell(1).getStringCellValue();
+         String sex=row.getCell(2).getStringCellValue();
+             double age=row.getCell(3).getNumericCellValue();
+         //             封装到对象中       
+         User user=new User();
+         user.setId((int) id);
+        user.setName(name);
+         user.setSex(sex);
+         user.setAge((int)age);
+        //             将对象添加数据库  
+        System.out.println(user);
+        }
 
+
+    }
 
     @Test
     public void terr() {
